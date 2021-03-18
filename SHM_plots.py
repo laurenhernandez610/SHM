1b7d651d156aa9760ec3f25be9824c22bc2f17f4
Lauren Hernandez <lauren.hernandez.m@gmail.com>
2021-03-18 15:13:04 -0500

    adding parameters

--- SHM_plots.py
+++ SHM_plots.py
@@ -2,19 +2,32 @@ from matplotlib.widgets import MultiCursor
 import matplotlib.pyplot as plt
 import numpy as np
 
-# This script sets up plots of oscillations over time
+# -----This script sets up plots of oscillations over time-----
+# Alternate, more concise way to plot: 
+# fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, sharex=False)
 
 fig = plt.figure()
-fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, sharex=True)
-t = np.arange(0.0, 3.0, 0.01)
+t = np.arange(0.0, 2.0, 0.01)
+
 ax1 = fig.add_subplot(311)
 ax1.plot(t, np.sin(0.9*np.pi*t))
+ax1.set_title("ax1", fontsize=9)
+
+ax2 = fig.add_subplot(312)
 ax2.plot(t, np.sin(4*np.pi*t))
+ax2.set_title("ax2", fontsize=9)
+
+ax3 = fig.add_subplot(313)
 ax3.plot(t, np.sin(8*np.pi*t))
+ax3.set_title("ax3", fontsize=9)
 
 multi = MultiCursor(fig.canvas, (ax1, ax2, ax3), color='y', lw=1,
                     horizOn=True, vertOn=True)
 
-st = fig.suptitle('Simple Harmonic Oscillations with Varying Parameters')
+fig.tight_layout()
+
+st = fig.suptitle('Simple Harmonic Oscillations with Varying Parameters', fontsize=10)
+st.set_y(0.95)
+fig.subplots_adjust(top=0.85)
 
 plt.show()
