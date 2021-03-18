3900474735f4ef32036397f322c2118d11f3d0b6
Lauren Hernandez <lauren.hernandez.m@gmail.com>
2021-03-18 14:56:12 -0500

    Added test comment

--- practice_plot.py
+++ practice_plot.py
@@ -1,7 +1,7 @@
 from matplotlib.widgets import MultiCursor
 import matplotlib.pyplot as plt
 import numpy as np
-
+# test
 fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True)
 t = np.arange(0.0, 2.0, 0.01)
 ax1.plot(t, np.sin(2*np.pi*t))
